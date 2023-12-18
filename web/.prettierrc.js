39034a83960d8bf3392f51844a1a971a401d118c
Ralph T <24992261+ralpht42@users.noreply.github.com>
2023-12-18 13:58:25 +0100

    feat: added webui with tailwind support

---
+++ web/.prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	trailingComma: 'all',
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
